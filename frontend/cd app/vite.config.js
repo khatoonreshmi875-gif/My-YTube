a16937905ca84db7198b03b321f0597bcfe1fb71
Reshmi <khatoonreshmi875@gmail.com>
2026-02-03 02:13:13 +0530

    update vite proxy and vercel.json frontend config

--- frontend/cd app/vite.config.js
+++ frontend/cd app/vite.config.js
@@ -1,3 +1,14 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     strictPort: true,
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -7,5 +18,15 @@ export default defineConfig({
   server: {
     port: 5173,
     strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
   },
 });
